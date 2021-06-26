3d4658a878780276a6972dbc684c00a0e7d6a046
poyun119 <38304165+poyun119@users.noreply.github.com>
2021-06-26 18:51:03 +0800

    change scooter detail

--- dev/js/detail.js
+++ dev/js/detail.js
@@ -1,23 +1,66 @@
-const detailImg = document.querySelector(".detailImg img");
-let src = detailImg.src
-const rotateImg = (e) => {
+// const rotateBlock = document.querySelector(".secondpic");
+const rotateImg = document.querySelector(".secondpic img");
+let src = rotateImg.src
+const rotate = (e) => {
+    e.stopPropagation()
     const path = "./images/scooter/"
     const x = e.offsetX;
-    const halfWidth = detailImg.clientWidth / 2;
+    const halfWidth = rotateImg.clientWidth / 2;
     if(x < halfWidth / 2){
-        detailImg.src = path + "/158X/158Xg_l.png";
+        rotateImg.src = path + "/158X/158Xg_l.png";
     }else if(x > halfWidth / 2 && x < halfWidth){
-        detailImg.src = path + "/158X/158-g.png"; 
-    }else if(x > halfWidth - 100 && x < halfWidth + 100){
-        detailImg.src = path + "/158X/158X銀背.png";
+        rotateImg.src = path + "/158X/158-g.png"; 
+    }else if(x > halfWidth - 50 && x < halfWidth + 50){
+        rotateImg.src = path + "/158X/158X銀背.png";
     }else{
-        detailImg.src = path + "158X/158Xg_2.png";
+        rotateImg.src = path + "158X/158Xg_2.png";
     }
 }
 
-detailImg.addEventListener("mousemove", rotateImg);
-detailImg.addEventListener("touchmove", rotateImg);
+rotateImg.addEventListener("mousemove", rotate);
+rotateImg.addEventListener("touchmove", rotate);
 
-detailImg.addEventListener("mouseout", function () {
-    detailImg.src = src
+rotateImg.addEventListener("mouseout", function () {
+    rotateImg.src = src
 })
+
+
+//swiper
+let swiper = new Swiper('.swiper-container', {
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    // loop: true,
+    breakpoints: {
+        1920: {
+            slidesPerView: 1,
+            spaceBetween: 60
+        },
+        1028: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        }
+    },
+    pagination: {
+        el: 'swiper-pagination'
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+
+//scooter changeColor
+const radioBtns = document.querySelectorAll(".radio");
+radioBtns.forEach(btn => btn.addEventListener("click", function(){
+    const imgArr = btn.dataset.scooter.split("==")
+    const swiperSlides = document.querySelectorAll(".swiper-img");
+    for(let i = 0; i < imgArr.length ; i++){
+        swiperSlides[i].src = imgArr[i]
+    }
+}))
